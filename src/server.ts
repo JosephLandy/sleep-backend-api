e572f1420b6cd8594badb74890fb49bd08ddb099
JosephLandy <joseph.landy.email@gmail.com>
2020-01-09 17:37:49 -0500

    added connection to atlas

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,13 @@ const dbName = 'mySleepDB';
 // const dbName='josephSleepDB';
 const dburl = `mongodb://localhost:27017/${dbName}`;
 
+// this worked seamlessly. Perfect.
+const atlasurl =
+  "mongodb+srv://temp-database-user:VFN6BKr6ESjEPmGN@jlcluster-sdzxg.azure.mongodb.net/test?retryWrites=true&w=majority";
+
 // connect to mongoDB, and start the app when it completes.
-mongoose.connect(dburl, { useNewUrlParser: true }).then(() => {
+
+mongoose.connect(atlasurl, { useNewUrlParser: true }).then(() => {
   app.listen(port, () => console.log(`App listening on port ${port}!`));
 }).catch((error => {
   console.log(`Mongoose initial connection error occured: ${error}`);
